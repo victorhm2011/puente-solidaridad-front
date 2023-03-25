9a2fa6c88f1b7dfaa1860266f4830cf3205d1e97
Victor Hugo Montaño <60761437+victorhm2011@users.noreply.github.com>
2023-03-25 19:29:55 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://puente-solidaridad-patient.herokuapp.com/v1/',
-  basePatientUrl: 'https://puente-solidaridad-patient.herokuapp.com/v1/',
-  basePhysicianUrl: 'https://puente-solidaridad-physician.herokuapp.com/v1/',
-  baseAidUrl: 'https://puente-solidaridad-aid.herokuapp.com/v1/',
-  baseSurgeryUrl: 'https://puente-solidaridad-surgery.herokuapp.com/v1/',
+  baseApiUrl: 'https://puente-solidaridad-api.herokuapp.com/v1/',
+  basePatientUrl: 'https://puente-solidaridad-patients.herokuapp.com/v1/',
+  basePhysicianUrl: 'https://puente-solidaridad-physicians.herokuapp.com/v1/',
+  baseAidUrl: 'https://puente-solidaridad-aids.herokuapp.com/v1/',
+  baseSurgeryUrl: 'https://puente-solidaridad-surgerys.herokuapp.com/v1/',
 };
